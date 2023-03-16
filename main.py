3a7445f114d29f570a0235342355017e192d3ba1
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-03-16 17:30:39 +0000

    Added update functionality along with comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Import necessary modules and classes
 from fastapi import FastAPI, HTTPException
 from typing import List 
-from models import User, Gender, Role
+from models import User, Gender, Role, UserUpdateDetails
 from uuid import UUID 
 
 # Create a FastAPI instance
@@ -39,18 +39,38 @@ async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
 
+# Define a DELETE endpoint at /users/{user_id}
 @app.delete("/users/{user_id}")
 async def delete_user(user_id: UUID):
+    # Loop through each user in the database
     for user in db:
+        # Check if the user's ID matches the given user_id
         if user.id == user_id:
+            # If there's a match, remove the user from the database
             db.remove(user)
+            # Return a success message
             return {"message": "User deleted successfully"}
-        raise HTTPException(status_code=404, detail=f"User ID:'{user_id}' not found")
+    # If no user is found with the given user_id, raise an HTTPException
+    raise HTTPException(status_code=404, detail=f"User ID:'{user_id}' not found")
     
+
+# Define a PUT endpoint at /users/{user_id}
 @app.put("/users/{user_id}")
-async def update_user(user_id: UUID, user: User):
-    for index, user in enumerate(db):
+async def update_user(user_update: UserUpdateDetails, user_id: UUID):
+    # Loop through each user in the database
+    for user in db:
+        # Check if the user's ID matches the given user_id
         if user.id == user_id:
-            db[index] = user
+            # If there's a match, update the user's information based on the provided UserUpdateDetails
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            # Return a success message
             return {"message": "User updated successfully"}
-        raise HTTPException(status_code=404, detail=f"User ID:'{user_id}' not found")
+    # If no user is found with the given user_id, raise an HTTPException
+    raise HTTPException(status_code=404, detail=f"User ID:'{user_id}' not found")
