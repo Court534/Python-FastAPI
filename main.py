9d7c2a91ff32a1c62301caeaba031bd5e25daf00
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-03-16 15:38:50 +0000

    Slight changes made and documented sections

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+# Import necessary modules and classes
 from fastapi import FastAPI
 from typing import List 
 from models import User, Gender, Role
 from uuid import uuid4
 
+# Create a FastAPI instance
 app = FastAPI()
 
+# Define a list of User objects with pre-defined attributes
 db: List[User] = [
-    User(id=uuid4(), 
+    User(id=UUID("8bafbadf-7564-44c6-a93d-6f44a70b3c26"), 
          first_name= "John",
          last_name= "Doe",
          gender= Gender.male,
          roles=[Role.student]
         ),
-    User(id=uuid4(), 
+    User(id=UUID("6708f9ad-064a-4c7d-8ad1-4986bfddb338"), 
          first_name= "Jannet",
          last_name= "Doe",
          gender= Gender.female,
@@ -20,10 +23,18 @@ db: List[User] = [
         )
 ]
 
+# Define a GET endpoint at root URL path that returns "Hello World"
 @app.get("/")
 async def root ():
     return {"Hello": "World"}
 
+# Define a GET endpoint at "/users" path that returns the db list of User objects
 @app.get("/users")
 async def fetch_users():
     return db
+
+# Define a POST endpoint at "/users" path that adds a new User object to db list
+@app.post("/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
--- models.py
+++ models.py
@@ -1,21 +1,25 @@
+# Import necessary modules and classes
 from typing import Optional, List
 from uuid import UUID, uuid4
 from pydantic import BaseModel
 from enum import Enum
 
+# Define an enumeration of gender values
 class Gender(str, Enum):
     male = "male"
     female = "female"
     
+# Define an enumeration of role values
 class Role(str, Enum):
     admin = "admin"
     user = "user"
     student = "student"
   
+# Define a User data model that inherits from the BaseModel class
 class User(BaseModel):
     id: Optional[UUID] = uuid4()
     first_name: str
-    last_name: str
-    middle_name: Optional[str]
-    gender: Gender
-    roles: List[Role]
+    last_name: str 
+    middle_name: Optional[str] = None 
+    gender: Gender 
+    roles: List[Role] 
